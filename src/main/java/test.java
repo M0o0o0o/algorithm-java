b631eb3daac6569373b602f1facd38eb5883b1ae
Lee-moo <dlandif22@gmail.com>
2022-07-08 21:43:37 +0900

    Iterator test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,12 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class test {
     public static void main(String[] args) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        queue.add(10);
+        queue.add(1);
+        Iterator<Integer> iter = queue.iterator();
+
 
     }
 
