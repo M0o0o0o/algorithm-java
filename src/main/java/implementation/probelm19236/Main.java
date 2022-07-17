069a9a80162bb745e937240bbc5324c7dafe7caf
Lee-moo <dlandif22@gmail.com>
2022-07-17 15:34:26 +0900

    Add 19236

--- src/main/java/implementation/probelm19236/Main.java
+++ src/main/java/implementation/probelm19236/Main.java
@@ -1,124 +1,151 @@
 package implementation.probelm19236;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
-public class Main {
-    // ↑, ↖, ←, ↙, ↓, ↘, →, ↗
 
-    static int n = 4;
-    static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
-    static int[][][] board = new int[4][4][2];
-    static int ans = 0;
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                board[i][j][0] = sc.nextInt();
-                board[i][j][1] = sc.nextInt() - 1;
+public class Main {
+    public static int[][] board;
+    public static Fish[] fish;
+    public static int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+    public static int[] dy = {0, -1, -1, -1, 0, 1, 1, 1};
+    public static int ans = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        board = new int[4][4];
+        fish = new Fish[17];
+        for(int i = 0; i < 4; i++) {
+            st  = new StringTokenizer(br.readLine(), " ");
+            for(int j = 0; j < 4; j++) {
+                int num = Integer.parseInt(st.nextToken());
+                int dir = Integer.parseInt(st.nextToken())-1;
+                fish[num] = new Fish(num, i, j, dir, 1);
+                board[i][j] = num;
             }
         }
 
+        int sx = 0, sy = 0;
+        int sd = fish[board[0][0]].dir;
+        int eat = board[0][0];
+        fish[board[0][0]].alive = 0;
+        board[0][0] = -1;
 
-        dfs(0, 0, board[0][0][0], board[0][0][1], board);
+        dfs(sx, sy, sd, eat);
 
         System.out.println(ans);
-
-        sc.close();
-
     }
 
-    public static void dfs(int x, int y, int total, int d, int[][][] board) {
-        board[x][y][0] = board[x][y][1] = -1;
+    public static void dfs(int sx, int sy, int sd, int eat) {
+        ans = Math.max(ans, eat);
 
-        // 물고기들의 이동
-        int[][][] nBoard = move(board, x, y);
 
-        System.out.println("물고기 이동 후 ");
-
-        for (int i = 0; i < 4; i++) {
-            for (int j = 0; j < 4; j++) {
-                System.out.print("(" + board[i][j][0] + "," + board[i][j][1] + ")" + "  ");
-            }
-            System.out.println();
-        }
-        System.out.println();
-        System.out.println();
-
-        // 상어 이동 가능한 지 확인 후에 가능하다면 리스트에 넣는다.
-        ArrayList<int[]> list = new ArrayList<>();
-        while (true) {
-            x += dx[d];
-            y += dy[d];
-
-            if (x >= 0 && x < n && y >= 0 && y < n) {
-                if(nBoard[x][y][0] != -1) list.add(new int[]{x, y});
-            } else break;
+        int[][] tempBoard = new int[board.length][board.length];
+        for(int i = 0; i < board.length; i++) {
+            System.arraycopy(board[i], 0, tempBoard[i], 0, board.length);
         }
 
-        if (list.size() == 0) {
-            ans = Math.max(ans, total);
-        }
 
-        // 상어 가능한 이동 경로 ㄱ
-        for (int[] next : list) {
-            int nx = next[0];
-            int ny = next[1];
-            dfs(nx, ny, total + nBoard[nx][ny][0], nBoard[nx][ny][1], nBoard);
+        Fish[] tempFish = new Fish[fish.length];
+        for(int i = 1; i <= 16; i++)
+            tempFish[i] = new Fish(fish[i].num, fish[i].x, fish[i].y, fish[i].dir, fish[i].alive);
 
-        }
-    }
 
-    public static int[][][] move(int[][][] board, int sx, int sy) {
-        System.out.println("move 현재 상어 위치 : " + sx + " " + sy);
+        moveFish();
 
-        int[][][] nBoard = new int[n][n][2];
-        HashMap<Integer, int[]> list = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                nBoard[i][j][0] = board[i][j][0];
-                nBoard[i][j][1] = board[i][j][1];
-                if (nBoard[i][j][0] != -1) {
-                    list.put(nBoard[i][j][0], new int[]{i, j});
-                }
+
+        for(int i = 1; i < 4; i++) {
+            int nx = sx + dx[sd] * i;
+            int ny = sy + dy[sd] * i;
+
+
+            if(nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && board[nx][ny] != 0) {
+                int eatFish = board[nx][ny];
+                int nd = fish[eatFish].dir;
+                board[sx][sy] = 0;
+                board[nx][ny] = -1;
+                fish[eatFish].alive = 0;
+
+                dfs(nx, ny, nd, eat+eatFish);
+
+                fish[eatFish].alive = 1;
+                board[sx][sy] = -1;
+                board[nx][ny] = eatFish;
             }
         }
 
-        for (int i = 1; i <= 16; i++) {
-            if(!list.containsKey(i)) continue;
-            int x = list.get(i)[0];
-            int y = list.get(i)[1];
-            int d = nBoard[x][y][1];
-            System.out.println(i + " " + d);
-            for (int j = 0; j < 8; j++) {
 
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if (nx >= 0 && nx < n && ny >= 0 && ny < n && (nx != sx || ny != sy)) {
-                    int nw = nBoard[nx][ny][0];
-                    int nd = nBoard[nx][ny][1];
-                    if (nw != -1) {
+        for(int j = 0; j < board.length; j++)
+            System.arraycopy(tempBoard[j], 0, board[j], 0, board.length);
 
-                        System.out.println(i + " " + nw  +"와 변경");
-                        System.out.println(d +" " + nx + " " + ny);
-                        System.out.println();
-                        list.get(nw)[0] = x;
-                        list.get(nw)[1] = y;
+        for(int i=1; i<=16; i++)
+            fish[i] = new Fish(tempFish[i].num, tempFish[i].x, tempFish[i].y, tempFish[i].dir, tempFish[i].alive);
+    }
+
+
+    public static void moveFish() {
+        for(int i = 1; i < 17; i++) {
+            if(fish[i].alive == 0) {
+                continue;
+            }
+
+            int cnt = 0;
+            int dir = fish[i].dir;
+            int nx = 0, ny = 0;
+
+            while(cnt < 8) {
+                dir %= 8;
+                fish[i].dir = dir;
+
+                nx = fish[i].x + dx[dir];
+                ny = fish[i].y + dy[dir];
+
+
+                if(nx >= 0 && nx < 4 && ny >= 0 && ny < 4 && board[nx][ny] != -1) {
+                    if(board[nx][ny] == 0) {
+                        board[fish[i].x][fish[i].y] = 0;
+                        fish[i].x = nx;
+                        fish[i].y = ny;
+                        board[nx][ny] = i;
+                    } else {
+
+                        int changeFish = fish[board[nx][ny]].num;
+                        fish[changeFish].x = fish[i].x;
+                        fish[changeFish].y = fish[i].y;
+                        board[fish[changeFish].x][fish[changeFish].y] = changeFish;
+
+
+                        fish[i].x = nx;
+                        fish[i].y = ny;
+                        board[nx][ny] = i;
                     }
-
-                    nBoard[nx][ny][0] = nBoard[x][y][0];
-                    nBoard[nx][ny][1] = nBoard[x][y][1];
-                    nBoard[x][y][0] = nw;
-                    nBoard[x][y][1] = nd;
-
                     break;
+                } else {
+                    dir++;
+                    cnt++;
                 }
-                d++;
-                if(d==8) d = 0;
             }
         }
+    }
 
+}
 
-        return nBoard;
+class Fish {
+    int num;
+    int x;
+    int y;
+    int dir;
+    int alive;
+
+    Fish(int num, int x, int y, int dir, int alive) {
+        this.num = num;
+        this.x = x;
+        this.y = y;
+        this.dir = dir;
+        this.alive = alive;
     }
 }
