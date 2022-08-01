7a1af01b78f72c8ae794929e2a64ad830f2566d1
Lee-moo <dlandif22@gmail.com>
2022-08-01 16:07:29 +0900

    Add problem 23807

--- src/main/java/dijkstra/problem23807/Main.java
+++ src/main/java/dijkstra/problem23807/Main.java
@@ -1,127 +1,111 @@
 package dijkstra.problem23807;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.StringTokenizer;
-
-class Node implements Comparable<Node>{
-    private int index;
-    private long distance;
-
-    public Node(int index, long distance) {
-        this.index = index;
-        this.distance = distance;
-    }
-
-    public int getIndex() {
-        return index;
-    }
-
-    public long getDistance() {
-        return distance;
-    }
-
-
-    @Override
-    public int compareTo(Node o) {
-        if (this.distance < o.distance) {
-            return -1;
-        }
-        return 1;
-    }
-}
-
+import java.util.*;
 
 public class Main {
-
-    public static final long INF = Long.MAX_VALUE - 10000000;
-    public static int n, m, start, end, p;
-    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
-    public static long[][] d = new long[102][100001];
-    public static int[] pList = new int[102];
-
-    public static void dijkstra(int start, long[] d) {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        q.offer(new Node(start, 0));
-        d[start] = 0;
-
-        while (!q.isEmpty()) {
-            Node node = q.poll();
-            long dist = node.getDistance();
-            int now = node.getIndex();
-
-            if(d[now] < dist) continue;
-
-            for (int i = 0; i < graph.get(now).size(); i++) {
-                int next = graph.get(now).get(i).getIndex();
-                long cost = graph.get(now).get(i).getDistance() + dist;
-
-                if (d[next] > cost) {
-                    d[next] = cost;
-                    q.offer(new Node(next, cost));
-                }
-            }
-
-        }
-    }
-
+    static int n, m, p, s, e;
+    static final long INF = 999876454321L;
+    static long ans;
+    static ArrayList<ArrayList<Edge>> graph = new ArrayList<>();
+    static HashMap<Integer, long[]> distance = new HashMap<>();
+    static int[] plist;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        long ans = INF;
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        ans = INF;
 
         for (int i = 0; i < n + 1; i++) {
-            graph.add(new ArrayList<Node>());
+            graph.add(new ArrayList<>());
         }
-
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            graph.get(a).add(new Node(b, c));
-            graph.get(b).add(new Node(a, c));
+            graph.get(a).add(new Edge(b, c));
+            graph.get(b).add(new Edge(a, c));
         }
         st = new StringTokenizer(br.readLine());
-        start = Integer.parseInt(st.nextToken());
-        end = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        e = Integer.parseInt(st.nextToken());
+        p = Integer.parseInt(br.readLine());
+        plist = new int[p];
         st = new StringTokenizer(br.readLine());
-        p = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < n+1; i++) {
-            Arrays.fill(d[i], INF);
+        distance.put(s, new long[n + 1]);
+        for (int i = 0; i < p; i++) {
+            int mid = Integer.parseInt(st.nextToken());
+            plist[i] = mid;
+            distance.put(mid, new long[n + 1]);
         }
 
-        dijkstra(start, d[0]);
-        st = new StringTokenizer(br.readLine());
-        for (int i = 1; i < p+1; i++) {
-            pList[i] = Integer.parseInt(st.nextToken());
-            dijkstra(pList[i], d[i]);
-        }
-        for (int i = 1; i < p+1; i++) {
-            for (int j = 1; j < p+1; j++) {
-                for (int k = 1; k < p+1; k++) {
-                    int a, b, c;
-                    a = pList[i];
-                    b = pList[j];
-                    c = pList[k];
-                    if(d[0][a] == INF ) continue;
-                    if(i == j || d[i][b] == INF) continue;
-                    if(i==k || j==k || d[j][c] == INF) continue;
-                    ans = Math.min(ans, d[0][a] + d[i][b] + d[j][c] + d[k][end]);
 
+        for (Integer mid : distance.keySet()) {
+            dijkstra(mid, distance.get(mid));
+        }
+
+
+        for (int i = 0; i < p; i++) {
+            for (int j = 0; j < p; j++) {
+                for (int k = 0; k < p; k++) {
+                    if(i == j || i == k || j == k) continue;
+                    ans = Math.min(ans, calc(plist[i], plist[j], plist[k]));
                 }
             }
         }
-        System.out.println(ans == INF ? -1 : ans);
+
+        if(ans == INF) System.out.println(-1);
+        else System.out.println(ans);
 
         br.close();
     }
+
+    static void dijkstra(int start, long[] distance) {
+        for (int i = 0; i < n + 1; i++) {
+            distance[i] = INF;
+        }
+        distance[start] = 0;
+        PriorityQueue<long[]> q = new PriorityQueue<>((o1, o2) -> {
+            if (o1[1] > o2[1]) return 1;
+            else if (o1[1] < o2[1]) return -1;
+            else return 0;
+        });
+        q.add(new long[]{start, 0});
+
+        while (!q.isEmpty()) {
+            long[] now = q.poll();
+            int node = (int) now[0];
+            long dist = now[1];
+            if(distance[node] < dist) continue;
+            for (Edge edge : graph.get(node)) {
+                long cost = dist + edge.cost;
+                if (cost < distance[edge.next]) {
+                    distance[edge.next] = cost;
+                    q.add(new long[]{edge.next, cost});
+                }
+            }
+        }
+    }
+
+    static class Edge {
+        int next;
+        long cost;
+        public Edge(int next, long cost) {
+            this.next = next;
+            this.cost = cost;
+        }
+    }
+
+    static long calc(int a, int b, int c) {
+        return distance.get(s)[a] + distance.get(a)[b] + distance.get(b)[c] + distance.get(c)[e];
+    }
 }
+
+
+
