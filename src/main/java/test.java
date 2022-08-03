0f1aa0281e6e7f65524fc13e2a839859de689e15
Lee-moo <dlandif22@gmail.com>
2022-08-03 09:55:32 +0900

    Test

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -4,22 +4,6 @@ import java.util.*;
 
 public class test {
     public static void main(String[] args) {
-        PriorityQueue<long[]> q = new PriorityQueue<>(new Comparator<long[]>() {
-            @Override
-            public int compare(long[] o1, long[] o2) {
-                if (o1[1] > o2[1]) return 1;
-                else if (o1[1] < o2[1]) return -1;
-                else return 0;
-            }
-        });
-        q.add(new long[]{0, 3});
-        q.add(new long[]{0, 1});
-        q.add(new long[]{0, 2});
-        System.out.println(Arrays.toString(q.poll()));
-        System.out.println(Arrays.toString(q.poll()));
-        System.out.println(Arrays.toString(q.poll()));
     }
-
-
 }
 
