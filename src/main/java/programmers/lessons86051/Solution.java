e14f80ee1a1b03d681764be6d2b907574476ad39
Lee-moo <dlandif22@gmail.com>
2022-09-10 10:11:11 +0900

    Add lessons 86051

---
+++ src/main/java/programmers/lessons86051/Solution.java
@@ -0,0 +1,9 @@
+package programmers.lessons86051;
+
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] numbers) {
+        return 45 - Arrays.stream(numbers).sum();
+    }
+}
