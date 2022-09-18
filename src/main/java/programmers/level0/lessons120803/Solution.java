256571bca6a0899590c17db86605cccb29529e3f
Lee-moo <dlandif22@gmail.com>
2022-09-18 10:11:11 +0900

    Add level0.lessons

---
+++ src/main/java/programmers/level0/lessons120803/Solution.java
@@ -0,0 +1,8 @@
+package programmers.level0.lessons120803;
+
+public class Solution {
+    public int solution(int num1, int num2) {
+        return num1 - num2;
+    }
+}
+
---
+++ src/main/java/programmers/level0/lessons120810/Solution.java
@@ -0,0 +1,7 @@
+package programmers.level0.lessons120810;
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
