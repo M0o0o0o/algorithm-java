f78a0372b0f41b4e950a51558b259e32d7b1055f
Lee-moo <dlandif22@gmail.com>
2022-10-01 16:33:03 +0900

    Add dev2022 test

---
+++ src/test/java/programmers/dev2022_3/SolutionTest.java
@@ -0,0 +1,15 @@
+package programmers.dev2022_3;
+
+import org.junit.jupiter.api.Test;
+import programmers.dev2022.dev2022_3.Solution;
+
+class SolutionTest {
+
+    @Test
+    public void test() {
+        Solution solution = new Solution();
+        System.out.println(solution.solution(6));
+
+    }
+
+}
