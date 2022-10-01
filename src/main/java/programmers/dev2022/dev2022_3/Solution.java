d2c564f16982a259498679113c2cba482cf26ad8
Lee-moo <dlandif22@gmail.com>
2022-10-01 16:32:46 +0900

    Add dev2022

---
+++ src/main/java/programmers/dev2022/dev2022_3/Solution.java
@@ -0,0 +1,10 @@
+package programmers.dev2022.dev2022_3;
+
+
+class Solution {
+    public long solution(int k) {
+        return 0l;
+    }
+
+
+}
--- src/main/java/test.java
+++ src/main/java/test.java
@@ -6,4 +6,3 @@ public class test {
     public static void main(String[] args) {
     }
 }
-
