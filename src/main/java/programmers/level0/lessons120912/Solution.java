e236b21a9860c813c2bed9da02a3ccb5aac5a1a6
Lee-moo <dlandif22@gmail.com>
2022-10-08 10:11:11 +0900

    Add 120912

---
+++ src/main/java/programmers/level0/lessons120912/Solution.java
@@ -0,0 +1,17 @@
+package programmers.level0.lessons120912;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+
+        for (int num : array) {
+            String numToStr = String.valueOf(num);
+            for (int i = 0; i < numToStr.length(); i++) {
+                if (numToStr.charAt(i) == '7') {
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
