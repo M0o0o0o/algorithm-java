67a70fbdddcbb1822af86bc1fd17425178c76606
Lee-moo <dlandif22@gmail.com>
2022-10-10 10:11:11 +0900

    Add 120913

---
+++ src/main/java/programmers/level0/lessons120913/Solution.java
@@ -0,0 +1,24 @@
+package programmers.level0.lessons120913;
+
+class Solution {
+    public String[] solution(String my_str, int n) {
+        StringBuilder sb = new StringBuilder();
+        int resultLen = my_str.length() / n + (my_str.length() % n > 0 ? 1 : 0);
+        String[] result = new String[resultLen];
+        int index = 0;
+        int cnt = 0;
+        for (char c : my_str.toCharArray()) {
+            sb.append(c);
+            cnt++;
+
+            if (cnt == n) {
+                result[index++] = sb.toString();
+                sb = new StringBuilder();
+                cnt = 0;
+            }
+        }
+        if(sb.length() > 0) result[index++] = sb.toString();
+        return result;
+    }
+
+}
