54d11b0069d76dd5f82cfab129a5ae1ff07c442a
Lee-moo <dlandif22@gmail.com>
2022-10-23 10:11:11 +0900

    Add lessons 120924

---
+++ src/main/java/programmers/lessons120924/Solution.java
@@ -0,0 +1,10 @@
+package programmers.lessons120924;
+
+class Solution {
+    public int solution(int[] common) {
+        if ((common[1] - common[0]) + common[1] == common[2]) {
+            return common[1] - common[0] + common[common.length-1];
+        }
+        return common[2] * (common[1] / common[0]);
+    }
+}
