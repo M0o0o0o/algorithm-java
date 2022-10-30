dd71ce7346bd277c73cb3f8dc00ddf541818d9d0
Lee-moo <dlandif22@gmail.com>
2022-10-30 10:11:11 +0900

    Add 87946

---
+++ src/main/java/programmers/lessons87946/Solution.java
@@ -0,0 +1,21 @@
+package programmers.lessons87946;
+
+class Solution {
+    int answer = -1;
+    public int solution(int k, int[][] dungeons) {
+        boolean[] visited = new boolean[dungeons.length];
+        dfs(k, visited, dungeons, 0);
+        return answer;
+    }
+
+    private void dfs(int k, boolean[] visited, int[][] dungeons, int cnt) {
+        answer = Math.max(cnt, answer);
+        for (int i = 0; i < dungeons.length; i++) {
+            if (k >= dungeons[i][0] && !visited[i]) {
+                visited[i] = true;
+                dfs(k - dungeons[i][1], visited, dungeons, cnt + 1);
+                visited[i] = false;
+            }
+        }
+    }
+}
