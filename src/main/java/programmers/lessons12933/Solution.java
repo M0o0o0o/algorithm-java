81723f39f93163ad4a0790bb0ed832a13886c1c3
Lee-moo <dlandif22@gmail.com>
2022-11-03 10:11:11 +0900

    Add 12933

---
+++ src/main/java/programmers/lessons12933/Solution.java
@@ -0,0 +1,26 @@
+package programmers.lessons12933;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+class Solution {
+    public long solution(long n) {
+        long answer = 0;
+        String str = String.valueOf(n);
+        List<String> list = new ArrayList<>();
+
+        for (int i = 0; i < str.length(); i++) {
+            list.add(String.valueOf(str.charAt(i)));
+        }
+
+        list.sort(Collections.reverseOrder());
+        for (int i = 0; i < list.size(); i++) {
+            answer += Integer.valueOf(list.get(i));
+            if (i < list.size() - 1) answer *= 10;
+        }
+
+        return answer;
+    }
+}
