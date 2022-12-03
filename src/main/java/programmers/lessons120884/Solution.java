e77c3cfa04f3a236d74e3888cb89eb742970a0ed
Lee-moo <dlandif22@gmail.com>
2022-12-03 10:11:11 +0900

    lessons 120884

---
+++ src/main/java/programmers/lessons120884/Solution.java
@@ -0,0 +1,12 @@
+package programmers.lessons120884;
+
+class Solution {
+    public int solution(int chicken) {
+        int ans = 0;
+        while (chicken >= 10) {
+            ans += chicken / 10;
+            chicken = (chicken / 10) + (chicken % 10);
+        }
+        return ans;
+    }
+}
