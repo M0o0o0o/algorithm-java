d1bddc7c2245d1a3e3dde6e98c18164ac79581c0
Lee-moo <dlandif22@gmail.com>
2023-01-02 10:11:11 +0900

    Add programmer problem

---
+++ src/main/java/programmers/lessons120883/Solution.java
@@ -0,0 +1,21 @@
+package programmers.lessons120883;
+
+
+class Solution {
+
+
+    public String solution(String[] id_pw, String[][] db) {
+
+        for (String[] strings : db) {
+            if (id_pw[0].equals(strings[0])) {
+                if (id_pw[1].equals(strings[1])) {
+                    return "login";
+                }
+                return "wrong pw";
+            }
+        }
+
+
+        return "fail";
+    }
+}
